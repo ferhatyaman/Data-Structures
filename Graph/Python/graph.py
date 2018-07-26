03151c6b40d3c26197bb86b7532164c7b97e39a4
ferhatyaman <ferhatyaman@sabanciuniv.edu>
2018-07-26 15:52:36 +0300

    New functions added

--- Graph/Python/graph.py
+++ Graph/Python/graph.py
@@ -3,30 +3,36 @@ class Graph:
     #Creates initilized graph, if not it creates empty graph
     def __init__(self, graphDict = None):
         if graphDict == None:
-          self.__graphDict = {}
+            self.__graphDict = {}
+            self.__vertices = []
+            self.__edges = []
         else:
             self.__graphDict = graphDict
+            self.__vertices = self.__graphDict.Keys()
+            self.__generate_edges()
     #add vertex if it is not in the graph
     def add_vertex(self, vertex):
-        if vertex not in self.__graphDict:
+        if vertex not in self.__vertices:
             self.__graphDict[vertex] = []
+            self.__vertices.append(vertex)
     #add edge if vertex is exits, if not add vertex to key then add edge to list 
     def add_edge(self, edge):
         (v1, v2) = tuple(edge)
         self.add_vertex(v1)
         self.add_vertex(v2)
         self.__graphDict[v1].append(v2)
+        self.__edges.append(tuple(edge))
     #get all vertices on the graph
     def vertices(self):
-        return list(self.__graphDict.Keys())
+        return self.__vertices
+    #Get all edges on the graph
     def edges(self):
-        return generate_edges()
+        return self.__edges
+    #Private function to generate all edges given graph
     def __generate_edges():
-        edges = []
         for key in self.__graphDict.Keys():
             for dest in self.__graphDict[key]:
-                edges.append((key,dest))
-        return edges
+                self.__edges.append((key,dest))
     
 if __name__ == "__main__":
 
