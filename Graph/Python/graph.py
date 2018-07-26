4b4f3a9b2b19575ad292e913b61e58c64ca1c846
Ferhat Yaman <ferhatyaman@Ferhats-MacBook-Pro.local>
2018-07-26 20:15:57 +0300

    BFS and DFS func added

--- Graph/Python/graph.py
+++ Graph/Python/graph.py
@@ -1,27 +1,29 @@
+# This class represents a directed graph
+# using adjacency list representation
 class Graph:
-    """Graph implementations with different constructors and algorithms"""
+    
     #Creates initilized graph, if not it creates empty graph
     def __init__(self, graphDict = None):
         if graphDict == None:
-            self.__graphDict = {}
+            self.__graph = {}
             self.__vertices = []
             self.__edges = []
         else:
-            self.__graphDict = graphDict
-            self.__vertices = self.__graphDict.Keys()
+            self.__graph = graphDict
+            self.__vertices = list(self.__graph.keys())
+            self.__edges = []
             self.__generate_edges()
     #add vertex if it is not in the graph
     def add_vertex(self, vertex):
         if vertex not in self.__vertices:
-            self.__graphDict[vertex] = []
+            self.__graph[vertex] = []
             self.__vertices.append(vertex)
     #add edge if vertex is exits, if not add vertex to key then add edge to list 
-    def add_edge(self, edge):
-        (v1, v2) = tuple(edge)
-        self.add_vertex(v1)
-        self.add_vertex(v2)
-        self.__graphDict[v1].append(v2)
-        self.__edges.append(tuple(edge))
+    def add_edge(self, u,v):
+        self.add_vertex(u)
+        self.add_vertex(v)
+        self.__graph[u].append(v)
+        self.__edges.append((u,v))
     #get all vertices on the graph
     def vertices(self):
         return self.__vertices
@@ -31,37 +33,52 @@ class Graph:
     #Private function to generate all edges given graph
     def __generate_edges(self):
         for key in self.__vertices:
-            for dest in self.__graphDict[key]:
+            for dest in self.__graph[key]:
                 self.__edges.append((key,dest))
 
+    #Returns BFS list of graph
     def bfs(self,start):
+        #Mark all vertex as visited
         visited = {v: False for v in self.__vertices}
-        queue = []
-        bfs_tree = []
-        queue.append(self.__graphDict[start])
-        visited[start] = True
-        bfs_tree.append(start)
-        
-        for el in queue:
-            if not visited[el]:
-                bfs_tree.append(el)
-                visited[el] = True
-                queue.append(self.__graphDict[el])
 
-        print (bfs_tree)
+        bfs_tree = [] 
+        #Setup first node
+        queue = [start]
+        visited[start] = True
+        
+        #iterate over queue
+        while queue:
+            #dequeue first node and put it bfs
+            el = queue.pop(0)
+            bfs_tree.append(el)
+            
+            #take all adjacency vertices into queue if is not visited
+            for ad in self.__graph[el]:
+                if not visited[ad]:
+                    queue.append(ad)
+                    visited[ad] = True
+    
+        return bfs_tree
+    
+    #Return list of DFS tree from given node
     def dfs(self,start):
         visited = {v: False for v in self.__vertices}
-        stack = []
+        
         dfs_tree = []
-        stack.append(self.__graphDict[start])
-        visited[start] = True
-        dfs_tree.append(start)
-        for el in stack:
-            if not visited[el]:
+        #Setup first node
+        stack = [start]
+
+        #iterate over stack and add vertices to stack if it is not visited
+        while stack:
+            el = stack.pop(-1)
             dfs_tree.append(el)
             visited[el] = True
-            stack.append(self.__graphDict[el])        
-        
+
+            for ad in self.__graph[el]:
+                if not visited[ad]:
+                    stack.append(ad)
+
+        return dfs_tree
 if __name__ == "__main__":
 
     g = { "a" : ["d"],
@@ -84,3 +101,14 @@ if __name__ == "__main__":
     print("Add vertex:")
     graph.add_vertex("z")
 
+    print("Add vertex:")
+    graph.add_vertex("z")
+
+    print("Vertices of graph:")
+    print(graph.vertices())
+
+    print("BFS of graph:")
+    print(graph.bfs('e'))
+
+    print("DFS of graph:")
+    print(graph.dfs('e'))
